8860da2684e5f59232e4d9ba235a790a897d0bf2
munnadroid <gotameraju2@gmail.com>
2013-03-06 16:23:53 +0545

    initial commit

---
+++ gen/com/munnadroid/example/clipboardactivity/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.munnadroid.example.clipboardactivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/munnadroid/example/clipboardactivity/Utility.java
@@ -0,0 +1,11 @@
+package com.munnadroid.example.clipboardactivity;
+
+import android.content.Context;
+import android.widget.Toast;
+ 
+public class Utility {
+ 
+    public static void showToastMessage(Context context, String message, int duration){
+        Toast.makeText(context, message, duration).show();
+    }
+}
